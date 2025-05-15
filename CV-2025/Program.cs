98a01f0e9ca1910f3c9a96c43ea704479ff3d18e
cristalvision <dorin_vieru@yahoo.com>
2025-05-15 14:47:17 +0300

    Update Program.cs

--- CV-2025/Program.cs
+++ CV-2025/Program.cs
@@ -1,1 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseWebSockets();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}");
+
+
+/*app.Map("/WebSocket", async (context) =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        //await WebSocketHander.ReceiveData(context, webSocket);
+        var array = new byte[] { 65, 66, 67, 68, 69 };
+        var segment = new ArraySegment<byte>(array, 0, 5);
+
+        var buffer = new byte[1024 * 4];
+        WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!result.CloseStatus.HasValue)
+        {
+
+        }//This loop keeps connection opened
+
+        //await webSocket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+});*/
+
+app.Run();
