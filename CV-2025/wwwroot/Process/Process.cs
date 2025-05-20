4dd54bbc49ac29d9e80739c6a53fb63883f9ba38
cristalvision <dorin_vieru@yahoo.com>
2025-05-21 00:02:46 +0300

    Update Process.cs

--- CV-2025/wwwroot/Process/Process.cs
+++ CV-2025/wwwroot/Process/Process.cs
@@ -14,7 +14,6 @@ namespace CV_2025.wwwroot.Process
         public WebSocketReceiveResult? result;
         public Page? page;
 
-
         MemoryMappedFile? mappedFile;
         MemoryMappedViewStream? viewStream;
         MemoryStream? memoryStream;
@@ -50,38 +49,7 @@ namespace CV_2025.wwwroot.Process
             await UpdateProgress("SVG", 30);
             page.Characters = new(page.bitmap256);//All characters have been removed
             XmlDocument document = page.ToSVG();
-
-            for (int y = 0; y < page.bitmap256.Height; y++)
-            {
-                for (int x = 0; x < page.bitmap256.Width; x++)
-                {
-                    int color = page.bitmap256.GetPixel(x, y);
-                    if (color == 255) continue;
-
-                    XmlElement rect = document.CreateElement("rect");
-                    rect.SetAttribute("width", "1");
-                    rect.SetAttribute("height", "1");
-                    rect.SetAttribute("x", x.ToString());
-                    rect.SetAttribute("y", y.ToString());
-                    rect.SetAttribute("fill", "black");
-                    document.ChildNodes[1].AppendChild(rect);
-                }
-            }
             //└─────────────────Bitmap to SVG─────────────────┘
-            //character1.Left = 421
-            //character1.Top = 38
-            //┌─────────Color first unknown character─────────┐
-            Character character1 = page.characters[0];
-            XmlElement rectangle = document.CreateElement("rect");
-            rectangle.SetAttribute("x", character1.Left.ToString());
-            rectangle.SetAttribute("y", character1.Top.ToString());
-            rectangle.SetAttribute("width", character1.Width.ToString());
-            rectangle.SetAttribute("height", character1.Height.ToString());
-            rectangle.SetAttribute("fill", "transparent");
-            rectangle.SetAttribute("stroke", "darkblue");
-            rectangle.InnerText = character1.value.ToString();
-            document.ChildNodes[1].AppendChild(rectangle);
-            //└─────────Color first unknown character─────────┘
 
 
             //┌─────────────────SVG to Stream─────────────────┐
@@ -106,25 +74,26 @@ namespace CV_2025.wwwroot.Process
 
         public async Task DisplayUnwnownChar()
         {
-            byte[] buffer = JsonSerializer.SerializeToUtf8Bytes(new dynamic[4] { "DisplayUnwnownChar", page.characters[2].Top, page.characters[2].Left, page.characters[2].Width });
+            byte[] buffer = JsonSerializer.SerializeToUtf8Bytes(new dynamic[4] { "DisplayUnwnownChar", page.unknownChars[2].Top, page.unknownChars[0].Left, page.unknownChars[0].DBWidth });
             await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
         }
 
-        public async Task UpdateDatabase(char value, int width)
+        public async Task UpdateDatabase(bool style, char value, int DBWidth)
         {
             if (value == '␀') return;
 
-            string tableName = width + "x" + 50;
+            string tableName = DBWidth + "x" + 50;
             if (!page.Characters.database.tableNames.Contains(tableName))
             {
-                string query = "CREATE TABLE " + tableName + " (ID AUTOINCREMENT PRIMARY KEY, `Black Pixels` Number, Charcater CHAR, Family CHAR, Style CHAR, CharSection VARBINARY);";
+                string query = "CREATE TABLE " + tableName + " (ID AUTOINCREMENT PRIMARY KEY, `Black Pixels` Number, Charcater CHAR(1), Family CHAR(15), Style CHAR(6), CharSection VARBINARY);";
                 //string MySQL = "CREATE TABLE " + tableName + " (ID int NOT NULL AUTO_INCREMENT, `Black Pixels` int NOT NULL, Value char(1) NOT NULL, Family varchar(15) NOT NULL, Style varchar(6), Section blob NOT NULL, PRIMARY KEY (ID))";
                 page.Characters.database.ExecuteNonQuery(query);
             }
 
-            byte[] section = page.characters[0].section;
+            byte[] section = page.unknownChars[0].section;
             page.Characters.database.tableName = tableName;
-            page.Characters.database.Insert(["Black Pixels", "Charcater", "Family", "Style", "CharSection"], new List<dynamic>() { Monochrome.CountBlackPixels(section), value, "Times New Roman", DBNull.Value, section });
+            dynamic fontStyle = (style) ? "Italic" : DBNull.Value;
+            page.Characters.database.Insert(["Black Pixels", "Charcater", "Family", "Style", "CharSection"], [Monochrome.CountBlackPixels(section), value, "Times New Roman", fontStyle, section]);
         }
     }
 }
