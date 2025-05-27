747ef3aecdef91e2cfb28760d1aec7d62bf19066
cristalvision <dorin_vieru@yahoo.com>
2025-05-27 13:42:14 +0300

    Update Database.cs

--- CV-2025/wwwroot/CristalVision/Database/Database.cs
+++ CV-2025/wwwroot/CristalVision/Database/Database.cs
@@ -1,12 +1,10 @@
-﻿using MySql.Data.MySqlClient;
-using MySql.Data.Types;
-using NetTopologySuite.Geometries;
-
 namespace CV_2025.CristalVision.Database
 {
     public abstract class CVDatabase
     {
         public abstract void ExecuteNonQuery(string query);
-        public abstract List<dynamic> Filter(string uniqueColumn, dynamic uniqueValue, byte[] data);
+        public abstract List<dynamic>? Filter(string uniqueColumn, dynamic uniqueValue);
+
+        public abstract void Close();
     }
 }
