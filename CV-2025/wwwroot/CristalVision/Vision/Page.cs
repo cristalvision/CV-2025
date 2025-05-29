63aad72772302cf0f336ca0a6f95e92d67f189fe
cristalvision <dorin_vieru@yahoo.com>
2025-05-29 23:08:16 +0300

    Update Page.cs

--- CV-2025/wwwroot/CristalVision/Vision/Page.cs
+++ CV-2025/wwwroot/CristalVision/Vision/Page.cs
@@ -1,4 +1,5 @@
 using CV_2025.CristalVision.Database;
+using System.Drawing;
 using System.Runtime.Versioning;
 using System.Xml;
 
@@ -17,16 +18,16 @@ namespace CV_2025.CristalVision.Vision
         Tables? Table;
 
         public List<Character> knownChars = [], unknownChars = [];
-        List<Word>? words;
-        List<Shape>? shapes = [];
-        List<Equation>? equations = [];
-        List<Table>? tables = [];
-        List<Paragraph>? Paragraphs = [];
+        public List<Word>? words;
+        public List<Shape>? shapes = [];
+        public List<Equation>? equations = [];
+        public List<Table>? tables = [];
+        public List<Paragraph>? Paragraphs = [];
 
         /// <summary>
         /// Distance factor to the next char relative to actual char width
         /// </summary>
-        private readonly Dictionary<char, float> distanceFactor = new() { { 'a', 100.0F }, { 'b', 1.0F }, { 'c', 100.0F }, { 'd', 3.7F }, { 'e', 3.7F }, { 'f', 100.0F }, { 'g', 100.0F }, { 'h', 1.0F }, { 'i', 2.1F }, { 'j', 100.0F }, { 'k', 100.0F }, { 'l', 2.1F }, { 'm', 100.0F }, { 'n', 3.5F }, { 'o', 1.0F }, { 'p', 1.0F }, { 'q', 1.0F }, { 'r', 100.0F }, { 's', 100.0F }, { 'ş', 100.0F }, { 't', 100.0F }, { 'ţ', 4.1F }, { 'u', 3.7F }, { 'v', 100.0F }, { 'w', 100.0F }, { 'x', 100.0F }, { 'y', 100.0F }, { 'z', 100.0F }, { 'A', 100.0F }, { 'B', 100.0F }, { 'D', 100.0F }, { 'E', 100.0F }, { 'O', 100.0F }, { 'T', 100.0F }, { 'V', 100.0F }, { 'α', 100.0F }, { '0', 100.0F }, { '2', 100.0F }, { '5', 100.0F }, { '6', 100.0F }, { '(', 100.0F }, { ')', 100.0F }, { ',', 100.0F }, { '/', 100.0F } };
+        static Dictionary<char, float> distanceFactor = new() { { 'a', 3.0F }, { 'b', 1.0F }, { 'c', 100.0F }, { 'd', 3.7F }, { 'e', 3.7F }, { 'f', 100.0F }, { 'g', 100.0F }, { 'h', 1.0F }, { 'i', 2.1F }, { 'j', 100.0F }, { 'k', 100.0F }, { 'l', 2.1F }, { 'm', 100.0F }, { 'n', 3.5F }, { 'o', 3.5F }, { 'p', 3.5F }, { 'q', 1.0F }, { 'r', 2.1F }, { 's', 100.0F }, { 'ş', 100.0F }, { 't', 2.0F }, { 'ţ', 4.1F }, { 'u', 3.7F }, { 'v', 100.0F }, { 'w', 100.0F }, { 'x', 100.0F }, { 'y', 100.0F }, { 'z', 3.7F }, { 'A', 100.0F }, { 'B', 100.0F }, { 'D', 100.0F }, { 'E', 100.0F }, { 'O', 100.0F }, { 'T', 100.0F }, { 'V', 100.0F }, { 'α', 100.0F }, { '0', 100.0F }, { '2', 100.0F }, { '5', 100.0F }, { '6', 100.0F }, { '(', 100.0F }, { ')', 100.0F }, { ',', 100.0F }, { '/', 100.0F } };
 
         public Page(MemoryStream? memoryStream)
         {
@@ -45,6 +46,83 @@ namespace CV_2025.CristalVision.Vision
             /// List of characters representing this word
             /// </summary>
             public List<char> value = [];
+
+            /// <summary>
+            /// List of Characters representing this word
+            /// </summary>
+            public List<Character> Characters = [];
+
+            /// <summary>
+            /// Get first word character relative to the reference
+            /// </summary>
+            public static Character GetFirstChar(List<Character> knownChars)
+            {
+                Character reference = knownChars[0];
+                
+                Character firstChar = new();
+                while (firstChar.value != '␀')
+                {
+                    float distance = reference.Width / distanceFactor[reference.value];//Max distance to the previous character
+                    List<Character> charsToLeft = [.. knownChars.Where(character => character.Right > reference.Left - distance && character.Right < reference.Left)];
+                    List<Character> prevChars = [.. charsToLeft.Where(character => character.Top < reference.Bottom && character.Bottom > reference.Top)];
+
+                    firstChar = (prevChars.Count > 0) ? prevChars[0] : new Character() { value = '␀' };
+
+                    if (firstChar.value != '␀')
+                    {
+                        reference = firstChar;
+                    }//Word continues to the left
+                    else
+                    {
+                        firstChar = reference;
+                        break;
+                    }//Start of word
+                }
+
+                return firstChar;
+            }
+
+            /// <summary>
+            /// Assembly characters in word chunks : loop to the right from reference
+            /// </summary>
+            public static Word GetWord(Character reference, List<Character> knownChars)
+            {
+                Word word = new() { Top = reference.Top, Left = reference.Left, Right = reference.Right, Bottom = reference.Bottom };
+                word.Characters.Add(reference);
+                word.value.Add(reference.value);
+                knownChars.Remove(reference);
+
+                Character nextChar = new();
+                while (nextChar.value != '␀')
+                {
+                    //float distance = reference.Width / distanceFactor[reference.value];//Max distance to the next character
+                    int distance = 10;
+                    List<Character> charsToRight = [.. knownChars.Where(character => character.Left < reference.Right + distance && character.Left > reference.Right)];
+                    List<Character> nextChars = [.. charsToRight.Where(character => character.Top < reference.Bottom && character.Bottom > reference.Top)];
+
+                    if (nextChars.Count == 0)
+                    {
+                        charsToRight = [.. knownChars.Where(character => character.Left == reference.Right || character.Left == reference.Right - 1 || character.Left == reference.Right - 2 || character.Left == reference.Right - 3)];
+                        nextChars = [.. charsToRight.Where(character => character.Top < reference.Bottom && character.Bottom > reference.Top)];
+                    }
+
+                    nextChar = (nextChars.Count > 0) ? nextChars[0] : new Character() { value = '␀' };
+
+                    if (nextChar.value == '␀')
+                        break;
+
+                    if (nextChar.Top < word.Top) word.Top = nextChar.Top;
+                    if (nextChar.Bottom > word.Bottom) word.Bottom = nextChar.Bottom;
+                    word.Right = nextChar.Right;
+
+                    word.Characters.Add(nextChar);
+                    word.value.Add(nextChar.value);
+                    knownChars.Remove(nextChar);
+                    reference = nextChar;
+                }
+
+                return word;
+            }
         }
         public struct Row
         {
@@ -99,47 +177,22 @@ namespace CV_2025.CristalVision.Vision
         }
 
         /// <summary>
-        /// Assembly characters in word chunks
+        /// Assembly chunks in words : loop to the left from reference
         /// </summary>
-        List<Word>? GetWordChunks()
-        {
-            List<Word>? wordChunks = [];
-            List<Character> knownChars = [.. this.knownChars];
-
-            while (knownChars.Count > 0)
-            {
-                Character thisChar = knownChars[0];
-                Word wordChunk = new();
-                wordChunk.value.Add(thisChar.value);
-                knownChars.Remove(thisChar);
-
-                while (thisChar.value != ' ')
-                {
-                    float distance = thisChar.Width / distanceFactor[thisChar.value];//Max distance to the next character
-                    List<Character> charsToRight = [.. knownChars.Where(character => character.Left < thisChar.Right + distance && character.Left > thisChar.Right)];
-                    List<Character> nextChars = [.. charsToRight.Where(character => character.Top < thisChar.Bottom && character.Bottom > thisChar.Top)];
-
-                    Character nextChar = (nextChars.Count > 0) ? nextChars[0] : new Character() { value = ' ' };
-
-                    if (nextChar.value == ' ')
-                        break;
-
-                    wordChunk.value.Add(nextChar.value);
-                    knownChars.Remove(nextChar);
-                    thisChar = nextChar;
-                }
-
-                wordChunks.Add(wordChunk);
-            }
-
-            return wordChunks;
-        }
-
         public void GetWords()
         {
-            List<Word>? wordChunks = GetWordChunks();
-            List<Word>? words = [];
+            List<Character>? knownChars = [.. this.knownChars];
+            
+            if (knownChars == null)
+                return;
 
+            words = [];
+            while (knownChars.Count > 0)
+            {
+                Character firstChar = Word.GetFirstChar(knownChars);
+                Word word = Word.GetWord(firstChar, knownChars);
+                words.Add(word);
+            }
 
         }
 
@@ -222,13 +275,8 @@ namespace CV_2025.CristalVision.Vision
 
             document.AppendChild(svg);
 
-            //┌─────────Color first unknown character─────────┐
-            List<Character> displayChars = [knownChars[0], knownChars[4]];
-            char firstChar = displayChars[0].value;//d
-            int right = displayChars[0].Right;//707
-            int width = displayChars[0].Width;//30
-            char secondChar = displayChars[1].value;//e
-            int left = displayChars[1].Left;//714
+            //┌────────Outline first unknown character────────┐
+            /*List<Character> displayChars = [knownChars[0], knownChars[4]];
 
             foreach (Character character in displayChars)
             {
@@ -241,8 +289,26 @@ namespace CV_2025.CristalVision.Vision
                 rectangle.SetAttribute("stroke", "darkblue");
                 rectangle.InnerText = character.value.ToString();
                 document.ChildNodes[1].AppendChild(rectangle);
+            }*/
+            //└────────Outline first unknown character────────┘
+
+            //┌─────────────────Outline words─────────────────┐
+            foreach (Word word in words)
+            {
+                int width = word.Right - word.Left;
+                int height = word.Bottom - word.Top;
+
+                XmlElement rectangle = document.CreateElement("rect");
+                rectangle.SetAttribute("x", word.Left.ToString());
+                rectangle.SetAttribute("y", word.Top.ToString());
+                rectangle.SetAttribute("width", width.ToString());
+                rectangle.SetAttribute("height", height.ToString());
+                rectangle.SetAttribute("fill", "transparent");
+                rectangle.SetAttribute("stroke", "darkblue");
+                rectangle.InnerText = word.value.ToString();
+                document.ChildNodes[1].AppendChild(rectangle);
             }
-            //└─────────Color first unknown character─────────┘
+            //└─────────────────Outline words─────────────────┘
 
             if (unknownChars.Count == 0)
                 return document;
