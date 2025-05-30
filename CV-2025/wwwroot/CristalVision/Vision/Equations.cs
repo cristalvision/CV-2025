134582ba89d72cfc980034f2c9ab77323474fb9c
cristalvision <dorin_vieru@yahoo.com>
2025-05-30 21:53:59 +0300

    Update Equations.cs

--- CV-2025/wwwroot/CristalVision/Vision/Equations.cs
+++ CV-2025/wwwroot/CristalVision/Vision/Equations.cs
@@ -1,11 +1,30 @@
+using static CV_2025.CristalVision.Vision.Page;
+
 namespace CV_2025.CristalVision.Vision
 {
     public struct Equation
     {
-
+        public enum Format { Fraction, Script, Integral, LargeOperator, Function, Accent, LimitAndLog, Operator, Matrix }
+        public Format format;
+        public List<Character> numerator, denominator;
     }
 
     public class Equations
     {
+        public static List<Equation> GetFractions(List<Character> unknownChars, List<Character> knownChars)
+        {
+            List<Character> lines = [.. unknownChars.Where(character => character.Width / character.Height > 10)];
+
+            foreach (Character line in lines)
+            {
+                List<Character> numerator = knownChars.Where(character => (line.Top - character.Bottom > 0) && (line.Top - character.Bottom < 50) && character.Left >= line.Left && character.Right <= line.Right).ToList();
+                List<Character> denominator = knownChars.Where(character => (character.Top - line.Bottom > 0) && (character.Top - line.Bottom < 50) && character.Left >= line.Left && character.Right <= line.Right).ToList();
+
+                Equation equation = new() { format = Equation.Format.Fraction, numerator = numerator, denominator = denominator };
+
+            }
+
+            return [];
+        }
     }
 }
