e097d85fb75e46966fdb0fa5aad9871910cceffd
cristalvision <dorin_vieru@yahoo.com>
2025-07-05 15:08:56 +0300

    Update SVG.cs

--- CV-2025/wwwroot/CristalVision/Output/SVG.cs
+++ CV-2025/wwwroot/CristalVision/Output/SVG.cs
@@ -43,86 +43,8 @@ namespace CV_2025.wwwroot.CristalVision.Output
 
             }//Place known characters as text
 
-
-            /*foreach (Character character in unknownChars)
-            {
-                for (int y = character.Top; y < character.Bottom; y++)
-                {
-                    for (int x = character.Left; x < character.Right; x++)
-                    {
-                        int color = bitmap256.GetPixel(x, y);
-                        if (color == 255) continue;
-
-                        XmlElement rect = document.CreateElement("rect");
-                        rect.SetAttribute("width", "1");
-                        rect.SetAttribute("height", "1");
-                        rect.SetAttribute("x", x.ToString());
-                        rect.SetAttribute("y", y.ToString());
-                        rect.SetAttribute("fill", "black");
-                        svg.AppendChild(rect);
-                    }
-                }
-            }//Place unknown characters as rectangle pixels*/
-
             document.AppendChild(svg);
-
-            //┌────────Outline first unknown character────────┐
-            /*List<Character> displayChars = [knownChars[0], knownChars[4]];
-
-            foreach (Character character in displayChars)
-            {
-                XmlElement rectangle = document.CreateElement("rect");
-                rectangle.SetAttribute("x", character.Left.ToString());
-                rectangle.SetAttribute("y", character.Top.ToString());
-                rectangle.SetAttribute("width", character.Width.ToString());
-                rectangle.SetAttribute("height", character.Height.ToString());
-                rectangle.SetAttribute("fill", "transparent");
-                rectangle.SetAttribute("stroke", "darkblue");
-                rectangle.InnerText = character.value.ToString();
-                document.ChildNodes[1].AppendChild(rectangle);
-            }*/
-            //└────────Outline first unknown character────────┘
-
-            //┌─────────────────Outline words─────────────────┐
-            /*foreach (Word word in words)
-            {
-                int width = word.Right - word.Left;
-                int height = word.Bottom - word.Top;
-
-                XmlElement rectangle = document.CreateElement("rect");
-                rectangle.SetAttribute("x", word.Left.ToString());
-                rectangle.SetAttribute("y", word.Top.ToString());
-                rectangle.SetAttribute("width", width.ToString());
-                rectangle.SetAttribute("height", height.ToString());
-                rectangle.SetAttribute("fill", "transparent");
-                rectangle.SetAttribute("stroke", "darkblue");
-                rectangle.InnerText = word.value.ToString();
-                document.ChildNodes[1].AppendChild(rectangle);
-            }*/
-            //└─────────────────Outline words─────────────────┘
-
-            /*if (page.unknownChars.Count == 0)
-                return document;*/
-
-            //┌─────────Color first unknown character─────────┐
-            /*Character character1 = unknownChars[0];
-            XmlElement rectangle = document.CreateElement("rect");
-            rectangle.SetAttribute("x", character1.Left.ToString());
-            rectangle.SetAttribute("y", character1.Top.ToString());
-            rectangle.SetAttribute("width", character1.Width.ToString());
-            rectangle.SetAttribute("height", character1.Height.ToString());
-            rectangle.SetAttribute("fill", "transparent");
-            rectangle.SetAttribute("stroke", "darkblue");
-            rectangle.InnerText = character1.value.ToString();
-            document.ChildNodes[1].AppendChild(rectangle);*/
-            //└─────────Color first unknown character─────────┘
-
-            //document.AppendChild(document.ChildNodes[1]);
-
             MemoryStream memoryStream = new();
-            /*byte[] filePDF = File.ReadAllBytes("C:\\Users\\user\\source\\repos\\CV-2025\\CV-2025\\wwwroot\\CristalVision\\Test Images\\TEST.pdf");
-            Stream stream = new MemoryStream(filePDF);
-            stream.CopyTo(memoryStream);*/
             document.Save(memoryStream);
 
             //┌─────────────────SVG to Stream─────────────────┐
