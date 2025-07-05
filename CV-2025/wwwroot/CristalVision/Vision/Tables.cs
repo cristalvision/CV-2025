08a04ea8ff2d802222ccf2b48e1b6120baa94359
cristalvision <dorin_vieru@yahoo.com>
2025-07-05 15:10:36 +0300

    Update Tables.cs

--- CV-2025/wwwroot/CristalVision/Vision/Tables.cs
+++ CV-2025/wwwroot/CristalVision/Vision/Tables.cs
@@ -1,11 +1,25 @@
+using CV_2025.CristalVision.Database;
+
 namespace CV_2025.CristalVision.Vision
 {
     public struct Table
     {
-
     }
 
     public class Tables
     {
+        Monochrome monochrome;
+
+        /// <summary>
+        /// Monochrome image as stream
+        /// </summary>
+        public Tables(Page page)
+        {
+            monochrome = page.monochrome;
+            /*database = new MySQL("cvtable");
+            this.monochrome = monochrome;
+            Size = monochrome.Size;
+            Content = monochrome.Content;*/
+        }
     }
 }
