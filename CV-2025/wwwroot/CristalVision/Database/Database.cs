c682c7ff0d252057268859ac11d0a39801239585
cristalvision <dorin_vieru@yahoo.com>
2025-07-05 15:06:16 +0300

    Update Database.cs

--- CV-2025/wwwroot/CristalVision/Database/Database.cs
+++ CV-2025/wwwroot/CristalVision/Database/Database.cs
@@ -1,7 +1,17 @@
+using CV_2025.CristalVision.Vision;
+using System.Configuration;
+using System.Text.Json;
+
 namespace CV_2025.CristalVision.Database
 {
     public abstract class CVDatabase
     {
+        public string? tableName;
+        public List<string> tableNames;
+        public enum Type { Acess, MySQL, SQLServer }
+        public static Type type;
+        public static string connectionString = String.Empty;
+        
         public abstract void ExecuteNonQuery(string query);
         public abstract List<dynamic>? Filter(string uniqueColumn, dynamic uniqueValue);
 
