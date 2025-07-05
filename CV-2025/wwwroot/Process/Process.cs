b65070cc44db33ea8cfd40900aeb92ffb94701c7
cristalvision <dorin_vieru@yahoo.com>
2025-07-05 15:12:42 +0300

    Update Process.cs

--- CV-2025/wwwroot/Process/Process.cs
+++ CV-2025/wwwroot/Process/Process.cs
@@ -1,3 +1,4 @@
+using CV_2025.CristalVision.Database;
 using CV_2025.CristalVision.Vision;
 using CV_2025.wwwroot.CristalVision.Output;
 using System.IO.MemoryMappedFiles;
@@ -25,13 +26,58 @@ namespace CV_2025.wwwroot.Process
             mappedFile = MemoryMappedFile.OpenExisting(mapName, MemoryMappedFileRights.ReadWrite);
             viewStream = mappedFile.CreateViewStream();
 
-            MemoryStream memoryStream = new();
-            viewStream.CopyTo(memoryStream);
+            MemoryStream source = new();
+            viewStream.CopyTo(source);
             //└──────────────────Read stream──────────────────┘
 
+            //=====TEST=====
+            /*const int WIDTH = 21;
+            const int AREA = WIDTH * 50;
+            string folderPath = "C:\\Users\\user\\source\\repos\\CV-2025\\CV-2025\\wwwroot\\CristalVision\\Characters\\" + WIDTH;
+            List<string> allFiles = [.. Directory.GetFiles(folderPath)];
+
+            List<byte[]> contents = [];
+            for (int index = 0; index < allFiles.Count; index++) 
+            {
+                string fileName = allFiles[index];
+                FileStream source = new(fileName, FileMode.Open);
+                
+                Bitmap256 bitmap256 = new(source);
+                contents.Add(bitmap256.GetSection2(WIDTH, 50));
+            }//Files to arrays
+
+            byte[] reference = new byte[AREA];
+            Array.Fill(reference, (byte)255);
+            for (int index = 0; index < (AREA); index++)
+            {
+                List<byte> pixels = [.. contents.Select(content => content[index])];
+                if (pixels.All(pixel => pixel == 0))
+                    reference[index] = 0;
+            }//Write reference pattern
+
+            Bitmap256 pattern = new(WIDTH, 50);
+            for (int index = 0; index < (AREA); index++)
+            {
+                int x = index % WIDTH;
+                int y = (index - x) / WIDTH;
+                pattern.SetPixel(x, y, reference[index]);
+            }
+
+            var database = new SQLServer("OCRDrawings");
+
+
+            //database.tableName = "dbo.Table_1";
+            //List<dynamic>? rows = database.Filter("Test1", "Test");
+
+
+            File.WriteAllBytes("C:\\Users\\user\\source\\repos\\CV-2025\\CV-2025\\wwwroot\\CristalVision\\Characters\\Pattern.bmp", pattern.Content);
+
+            string breakPoint = null;*/
+            //=====TEST=====
 
             //┌─────────────Pixels to structures──────────────┐
-            page = new(memoryStream);
+            page = new(source, CVDatabase.Type.Acess, "Provider=Microsoft.ACE.OLEDB.16.0;Data Source=" + Directory.GetCurrentDirectory() + "\\wwwroot\\CristalVision\\Database\\cvcharacters.accdb");
+            //page = new(source, CVDatabase.Type.SQLServer, "Server=DESKTOP-IUG3E2G\\MSSQLSERVER01;Database=OCRDrawings;Trusted_connection=True;TrustServerCertificate=True;");
             
             await UpdateProgress("Get Text", 30);
             page.GetCharacters();
@@ -49,16 +95,16 @@ namespace CV_2025.wwwroot.Process
 
 
             //┌─────────────────Bitmap to SVG─────────────────┐
-            await UpdateProgress("SVG", 30);
-            page.Characters = new(page.bitmap256);//All characters have been removed
+            await UpdateProgress("SVG", 75);
+            page.Characters = new(page);//All characters have been removed
             SVG SVG = new(page);
             memoryStream = SVG.ToMemeoryStream(mapName);
 
-            PDF PDF = new PDF(page);
+            PDF PDF = new(page);
             PDF.Save();
 
-            Word word = new Word(page);
-            word.Save();
+            /*Word word = new Word(page);
+            word.Save();*/
             //└─────────────────Bitmap to SVG─────────────────┘
 
             byte[] buffer = JsonSerializer.SerializeToUtf8Bytes(new dynamic[2] { "Start", "Image/Display?Name=" + mapName + " - SVG&contentType=image/svg+xml&length=" + memoryStream.Length });
@@ -66,35 +112,10 @@ namespace CV_2025.wwwroot.Process
             await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
         }
 
-        public async Task UpdateProgress(string action, dynamic value)
+        public async Task UpdateProgress(string descriptiion, dynamic value)
         {
-            byte[] buffer = JsonSerializer.SerializeToUtf8Bytes(new dynamic[3] { "Progress", action, value });
+            byte[] buffer = JsonSerializer.SerializeToUtf8Bytes(new dynamic[3] { "Progress", descriptiion, value });
             await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
-        }
-
-        public async Task DisplayUnwnownChar()
-        {
-            byte[] buffer = JsonSerializer.SerializeToUtf8Bytes(new dynamic[4] { "DisplayUnwnownChar", page.unknownChars[2].Top, page.unknownChars[0].Left, page.unknownChars[0].DBWidth });
-            await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
-        }
-
-        public async Task UpdateDatabase(bool style, char value, int DBWidth)
-        {
-            if (value == '␀') return;
-            
-            string tableName = DBWidth + "x" + 50;//"88"
-            if (!page.Characters.database.tableNames.Contains(tableName))
-            {
-                string query = "CREATE TABLE " + tableName + " (ID AUTOINCREMENT PRIMARY KEY, `Black Pixels` Number, Charcater CHAR(1), Family CHAR(15), Style CHAR(6), CharSection LONGBINARY);";
-                //string MySQL = "CREATE TABLE " + tableName + " (ID int NOT NULL AUTO_INCREMENT, `Black Pixels` int NOT NULL, Value char(1) NOT NULL, Family varchar(15) NOT NULL, Style varchar(6), Section blob NOT NULL, PRIMARY KEY (ID))";
-                page.Characters.database.ExecuteNonQuery(query);
-            }
-
-            byte[] section = page.unknownChars[0].section;
-            page.Characters.database.tableName = tableName;
-            dynamic fontStyle = (style) ? "Italic" : DBNull.Value;
-            int blackPixels = Monochrome.CountBlackPixels(section);//1800
-            page.Characters.database.Insert(["Black Pixels", "Charcater", "Family", "Style", "CharSection"], [Monochrome.CountBlackPixels(section), value, "Times New Roman", fontStyle, section]);
         }
     }
 }
